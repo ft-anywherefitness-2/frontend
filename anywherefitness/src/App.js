f39062d62cfb7fc89f5ca6c4dcb572bf8bd074d9
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-07-26 13:41:32 -0700

    Navbar created

--- anywherefitness/src/App.js
+++ anywherefitness/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import NavBar from './components/NavBar';
+import HomePage from './components/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <Router>
+        <NavBar />
+     
+
+    <div className="pages">
+      <Switch>
+         {/* component={Signup} */}
+         <Route exact path="/signup" />
+         {/* component={Login} */}
+         <Route exact path="/login" />
+        
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
+    </Router>
+    </>
   );
 }
 
