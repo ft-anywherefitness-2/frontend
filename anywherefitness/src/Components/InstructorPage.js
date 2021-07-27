0a27e786888152be1afe720d2e54ed0fba1217c7
Bradly <bradlylewisdev@gmail.com>
2021-07-27 19:11:11 -0400

    create class block

--- anywherefitness/src/Components/InstructorPage.js
+++ anywherefitness/src/Components/InstructorPage.js
@@ -1,34 +1,47 @@
 import React, { useState } from 'react'
 import '../Styles/InstructorPage.css'
+import dummyData from '../dummy-data/classes.js'
+
 
 
 
 const InstructorPage = () => {
-    const [blank, setBlank] = useState('')
+    const [classes] = useState(dummyData)
+    console.log(classes)
+
+    // breaks when you name argument 'class'
+    const getClasses = () => {
+        return (
+            <div className='class-container'>
+            {classes.map((item) => {
+                return (
+                        <div className='class'>
+                            <div className='class-content-container'>
+                                <div className='class-header'>
+                                    <h5>{item.type}</h5>
+                                    <h4>{item.capacity}</h4>
+                                </div>
+                                <div className='text-box'>
+                                    <a href="#" class="btn btn-white btn-animate">click me</a>
+                                </div>
+                            </div>
+                        </div>
+                        )
+                })}
+            </div>
+        )
+    }
+
+
+
+
+
 
 
     return (
-        <div className='class-container'>
-
-
-            <div className='class'>
-                <div className='class-content-container'>
-                    <div className='class-header'>
-                        <h4>Yoga</h4>
-                        <h5>25</h5>
-                    </div>
-
-                    <div class="text-box">
-                        <a href="#" class="btn btn-white btn-animate">click me</a>
-                    </div>
-
-                </div>
-            </div>
-
-
-
+        <div className='instructor-page-container'>
+            {getClasses()}
         </div>
-        
     )
 }
 
