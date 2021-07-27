5369f7a8aa2bdcb08e4733f73eeb5a1c773b7118
Bradly <bradlylewisdev@gmail.com>
2021-07-27 15:30:51 -0400

    personalize props to components

--- anywherefitness/src/App.js
+++ anywherefitness/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import NavBar from './Components/NavBar.js'
@@ -7,13 +6,8 @@ import HomePage from './Components/HomePage.js'
 import Login from './Components/Login.js'
 import SignUp from './Components/SignUp.js'
 
-const initialValues = {name:'', username: '', password: '', role: '', type:'', 'startTime': '', duration: '', intensityLevel: '', location: '', registeredAttendees: '', maxClassSize: '', classType: '', date: '' }
-
 function App() {
-  const [formValues, setFormValues] = useState(initialValues)
 
-  // HANDLERS /////////////////////////////////
-  const inputChange = (name, value) => setFormValues({ ...formValues, [name]: value })
 
   return (
     <>
@@ -23,16 +17,7 @@ function App() {
 
     <div className="pages">
       <Switch>
-        <Route exact path="/signup" component={SignUp}   >
-          <SignUp
-            formValues={formValues}
-            change={inputChange}
-            // submit={}
-            // disabled={}
-            // errors={}
-          />
-          </Route>
-        
+        <Route exact path="/signup" component={SignUp} />
         <Route exact path="/login" component={Login}/>
         <Route exact path="/" component={HomePage} />
       </Switch>
