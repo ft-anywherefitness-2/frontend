8c5e08ef8c4e80ecde2176cc06da677c8af9f0bd
Bradly <bradlylewisdev@gmail.com>
2021-07-27 20:15:04 -0400

    update mvp

---
+++ anywherefitness/src/Components/ClientClass.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import dummyData from '../dummy-data/classes.js'
+
+// breaks when you name argument 'class'
+const ClientClasses = () => {
+    const [classes] = useState(dummyData)
+
+    return (
+        <div className='class-container'>
+            {classes.map((item) => {
+                return (
+                <div className='class instructor-class'>
+                    <div className='class-content-container'>
+                        <div className='class-header'>
+                            <h5>{item.type}</h5>
+                            <h4>{item.capacity}</h4>
+                        </div>
+                        <div className='text-box'>
+                            <a href="#" class="btn btn-white btn-animate">click me</a>
+                        </div>
+                    </div>
+                </div>
+                )
+        })}
+    </div>
+)
+}
+
+export default GetClasses
--- anywherefitness/src/Components/InstructorPage.js
+++ anywherefitness/src/Components/InstructorPage.js
@@ -1,46 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../Styles/InstructorPage.css'
-import dummyData from '../dummy-data/classes.js'
-
-
-
+import InstructorClasses from './InstructorClass.js'
 
 const InstructorPage = () => {
-    const [classes] = useState(dummyData)
-    console.log(classes)
-
-    // breaks when you name argument 'class'
-    const getClasses = () => {
-        return (
-            <div className='class-container'>
-            {classes.map((item) => {
-                return (
-                        <div className='class'>
-                            <div className='class-content-container'>
-                                <div className='class-header'>
-                                    <h5>{item.type}</h5>
-                                    <h4>{item.capacity}</h4>
-                                </div>
-                                <div className='text-box'>
-                                    <a href="#" class="btn btn-white btn-animate">click me</a>
-                                </div>
-                            </div>
-                        </div>
-                        )
-                })}
-            </div>
-        )
-    }
-
-
-
-
-
+    
 
 
     return (
         <div className='instructor-page-container'>
-            {getClasses()}
+            <InstructorClasses />
         </div>
     )
 }
